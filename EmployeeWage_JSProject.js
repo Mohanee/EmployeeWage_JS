df93c53fa120a04dcc01222fd7329d38880ad765
Mohanee <mohanee99@gmail.com>
2020-11-19 17:33:52 +0530

    [mohanee]Add method to CheckAbsence of employee

--- EmployeeWage_JSProject.js
+++ EmployeeWage_JSProject.js
@@ -1,1 +1,12 @@
 //Hello Welcome to EmployeeWage Management project in JavaScript
+
+//UC1 CheckAbsence
+const IS_ABSENT =0;
+let empCheck = Math.floor(Math.random()*10) %2;
+if(empCheck == IS_ABSENT)
+{
+    console.log("Employee is Absent");
+}
+else{
+    console.log("Employee is present");
+}
